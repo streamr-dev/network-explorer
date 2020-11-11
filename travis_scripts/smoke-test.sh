a46b39bb1ccfe9741fd0e9ef76c44094e90ceace
Tuomas Koponen <tuomas.koponen@streamr.com>
2020-11-11 10:28:07 +0200

    Fix wait time

--- travis_scripts/smoke-test.sh
+++ travis_scripts/smoke-test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-docker run -d --rm -p3000:3000 --name=network-explorer $OWNER/$IMAGE_NAME:local
+docker run -d -p3000:3000 --name=network-explorer $OWNER/$IMAGE_NAME:local
 
 ## Wait for the service to come online and test
-wait_time=10;
+wait_time=20;
 for (( i=0; i < 5; i=i+1 )); do
     if ! curl -s http://localhost:3000/; then
         echo "Attempting to connect to network-explorer, retrying in $wait_time seconds"
