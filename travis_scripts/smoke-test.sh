f6e15bac28c0646a83ffff42ef524c503856c2c6
Tuomas Koponen <tuomas.koponen@streamr.com>
2020-11-11 12:25:10 +0200

    Try static ip

--- travis_scripts/smoke-test.sh
+++ travis_scripts/smoke-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-docker run -d -p3000:3000 --name=network-explorer -e HOST=0.0.0.0 $OWNER/$IMAGE_NAME:local
+docker run -d -p 127.0.0.1:3000:3000 --name=network-explorer -e HOST=127.0.0.1 $OWNER/$IMAGE_NAME:local
 
 ## Wait for the service to come online and test
 wait_time=20;
 for (( i=0; i < 5; i=i+1 )); do
-    if ! curl --silent --show-error http://localhost:3000/; then
+    if ! curl --silent --show-error http://127.0.0.1:3000/; then
         docker ps -a
         docker logs --tail 20 network-explorer
         echo "Attempting to connect to network-explorer, retrying in $wait_time seconds"
