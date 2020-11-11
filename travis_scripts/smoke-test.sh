c2b8670cd27bffe41e9bd725085276ea18b0be8d
Tuomas Koponen <tuomas.koponen@streamr.com>
2020-11-11 13:03:24 +0200

    Cleanup

--- travis_scripts/smoke-test.sh
+++ travis_scripts/smoke-test.sh
@@ -4,11 +4,9 @@ set -e
 docker run -dit -p 3000:3000 --name=network-explorer $OWNER/$IMAGE_NAME:local
 
 ## Wait for the service to come online and test
-wait_time=20;
+wait_time=10;
 for (( i=0; i < 5; i=i+1 )); do
     if ! curl --silent --show-error http://localhost:3000/; then
-        docker ps -a
-        docker logs --tail 20 network-explorer
         echo "Attempting to connect to network-explorer, retrying in $wait_time seconds"
         sleep $wait_time
         wait_time=$(( 2*wait_time ))
