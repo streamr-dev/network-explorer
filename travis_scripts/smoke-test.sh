c52bfe9da22bc7ca6e5cdf66fc853e7be6bee3ec
Tuomas Koponen <tuomas.koponen@streamr.com>
2020-11-11 14:39:55 +0200

    [FRONT-136] Add Dockerfile and Travis configuration (#16)
    * Add Dockerfile and Travis build configuration

---
+++ travis_scripts/smoke-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+docker run -dit -p 3000:3000 --name=network-explorer $OWNER/$IMAGE_NAME:local
+
+## Wait for the service to come online and test
+wait_time=10;
+for (( i=0; i < 5; i=i+1 )); do
+    if ! curl --silent --show-error http://localhost:3000/; then
+        echo "Attempting to connect to network-explorer, retrying in $wait_time seconds"
+        sleep $wait_time
+        wait_time=$(( 2*wait_time ))
+    else
+        exit 0
+    fi
+done
+exit 1
