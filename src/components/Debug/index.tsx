6288ef14504bd642d325d6a76457865d7c7d510a
Juha Haavisto <juha.haavisto@sujuwa.fi>
2021-07-29 14:00:37 +0300

    Make debug window resizable

--- src/components/Debug/index.tsx
+++ src/components/Debug/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useEffect, useReducer, useRef } from 'react'
 import styled from 'styled-components/macro'
 import JsonView from 'react-pretty-json'
 
@@ -10,7 +10,7 @@ import {
 import { isLocalStorageAvailable } from '../../utils/storage'
 
 export const APP_DEBUG_MODE_KEY = 'network-explorer.debug'
-export const APP_DEBUG_WINDOW_POS_KEY = 'network-explorer.debug_window_pos'
+export const APP_DEBUG_WINDOW_PROPS_KEY = 'network-explorer.debug_window_props'
 const storage = isLocalStorageAvailable() ? window.localStorage : null
 
 export function getDebugMode() {
@@ -29,15 +29,30 @@ type WindowPos = {
   y: number,
 }
 
-export function getDebugWindowPos(): WindowPos {
-  return !!storage && JSON.parse(storage.getItem(APP_DEBUG_WINDOW_POS_KEY) || '{"x":0,"y":0}')
+type WindowDimension = {
+  width: number,
+  height: number,
 }
 
-export function setDebugWindowPos(value: WindowPos) {
+type WindowProps = WindowPos & WindowDimension
+
+const initialState: WindowProps = {
+  x: 0,
+  y: 0,
+  width: 320,
+  height: 200,
+}
+
+export function getDebugWindowProps(): WindowProps {
+  return !!storage && JSON.parse(storage.getItem(APP_DEBUG_WINDOW_PROPS_KEY) ||
+    JSON.stringify(initialState))
+}
+
+export function setDebugWindowProps(value: WindowProps) {
   if (!storage) {
     return
   }
-  storage.setItem(APP_DEBUG_WINDOW_POS_KEY, JSON.stringify(value))
+  storage.setItem(APP_DEBUG_WINDOW_PROPS_KEY, JSON.stringify(value))
 }
 
 const DebugContainer = styled.div`
@@ -74,19 +89,32 @@ const DebugContainer = styled.div`
 `
 
 const Wrapper = styled.div`
-  width: 320px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const Header = styled.div`
+  background-color: #323232;
+  color: white;
+  padding: 4px 8px;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  cursor: grab;
 `
 
 const Variables = styled.pre`
   overflow: scroll;
   display: block;
-  height: 200px;
+  flex: 1;
   font-size: 8px;
   padding: 5px 10px;
   margin: 0;
   background-color: rgba(0, 0, 0, 0.3);
   font-family: ${MONO};
-  border-radius: 4px;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
 
   .key {
     font-weight: strong;
@@ -106,15 +134,32 @@ const Variables = styled.pre`
   }
 `
 
+const ResizeHandle = styled.div`
+  position: absolute;
+  right: 8px;
+  bottom: 8px;
+  z-index: 10002;
+  cursor: nwse-resize;
+  width: 0;
+  height: 0;
+  border-bottom: 16px solid #323232;
+  border-left: 16px solid transparent;
+  opacity: 0.8;
+`
+
 const isDebugEnabled = getDebugMode()
 
 const Debug = () => {
   const { pending } = useAllPending()
   const { store } = useStore()
-  const [pos, setPos] = useState<WindowPos>(getDebugWindowPos())
+  const [windowProps, setWindowProps] = useReducer(
+    (prevState: WindowProps, nextState: WindowPos | WindowDimension) => ({
+      ...(prevState || {}),
+      ...nextState,
+    }), getDebugWindowProps())
   const ref = useRef<HTMLDivElement>(null)
 
-  const onMouseDown = ({ clientX: x0, clientY: y0, button }: React.MouseEvent<HTMLElement>) => {
+  const onMoveContainer = ({ clientX: x0, clientY: y0, button }: React.MouseEvent<HTMLElement>) => {
     const { current: el } = ref
 
     if (button !== 0 || !el) { return }
@@ -124,14 +169,14 @@ const Debug = () => {
     const onMove = (e: MouseEvent) => {
       e.preventDefault()
 
-      setPos({
+      setWindowProps({
         x: offsetLeft - (x0 - e.clientX),
         y: offsetTop - (y0 - e.clientY),
       })
     }
 
     const onUp = (e: MouseEvent) => {
-      setDebugWindowPos({
+      setWindowProps({
         x: offsetLeft - (x0 - e.clientX),
         y: offsetTop - (y0 - e.clientY),
       })
@@ -144,6 +189,44 @@ const Debug = () => {
     window.addEventListener('mouseup', onUp)
   }
 
+  const onResizeContainer = ({
+    clientX: x0,
+    clientY: y0,
+    button,
+  }: React.MouseEvent<HTMLElement>) => {
+    const { current: el } = ref
+
+    if (button !== 0 || !el) { return }
+
+    const { offsetWidth, offsetHeight } = el || {}
+
+    const onMove = (e: MouseEvent) => {
+      e.preventDefault()
+
+      setWindowProps({
+        width: offsetWidth - (x0 - e.clientX),
+        height: offsetHeight - (y0 - e.clientY),
+      })
+    }
+
+    const onUp = (e: MouseEvent) => {
+      setWindowProps({
+        width: offsetWidth - (x0 - e.clientX),
+        height: offsetHeight - (y0 - e.clientY),
+      })
+
+      window.removeEventListener('mousemove', onMove)
+      window.removeEventListener('mouseup', onUp)
+    }
+
+    window.addEventListener('mousemove', onMove)
+    window.addEventListener('mouseup', onUp)
+  }
+
+  useEffect(() => {
+    setDebugWindowProps(windowProps)
+  }, [windowProps])
+
   // Hide debug view if not enabled in local storage
   if (!isDebugEnabled) {
     return null
@@ -151,14 +234,20 @@ const Debug = () => {
 
   return (
     <DebugContainer
-      onMouseDown={onMouseDown}
       ref={ref}
       style={{
-        left: `${pos.x}px`,
-        top: `${pos.y}px`,
+        left: `${windowProps.x}px`,
+        top: `${windowProps.y}px`,
+        width: `${windowProps.width}px`,
+        height: `${windowProps.height}px`,
       }}
     >
       <Wrapper>
+        <Header
+          onMouseDown={onMoveContainer}
+        >
+          Debug
+        </Header>
         <Variables>
           <JsonView
             json={{
@@ -168,6 +257,9 @@ const Debug = () => {
           />
         </Variables>
       </Wrapper>
+      <ResizeHandle
+        onMouseDown={onResizeContainer}
+      />
     </DebugContainer>
   )
 }
