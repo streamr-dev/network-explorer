c5e0ac719e64b9ed37b3880ac81ac9a43f52c112
Mariusz Roliński <mariusz@mondoreale.pl>
2024-03-17 15:46:58 +0100

    Move `App` to `components` folder`
    After all, it's a component, eh?

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import '@ibm/plex/css/ibm-plex.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './app/App'
+import { App } from './components/App'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(<App />)
